dcc4d4e4e0abbfc019a3a6615b44f4bebee207a8
Vicky Law <vicky.law1217@gmail.com>
2019-09-15 12:04:21 -0400

    added form validation in servey html and update GitHub Repo in home html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,39 +1,39 @@
 let friends = [
     {
         name: "Biily White",
-        photo: "https://static.boredpanda.com/blog/wp-content/uploads/2015/11/maine-coon-cats-19__605.jpg",
+        photo: "https://breedscat.com/wp-content/uploads/2016/10/maine-coon-3-300x242.jpg",
         scores: [1, 1, 5, 5, 3, 3, 3, 4, 5, 1]
     },
     {
         name: "Amy Spencer",
-        photo: "https://mymodernmet.com/wp/wp-content/uploads/2019/01/maine-coon-cat-photography-26.jpg",
+        photo: "https://fastly.4sqi.net/img/general/200x200/391523811_aff-VZcb-8ymhhNolNXzts0oAf6K2Ltxd_iuNwxdjlg.jpg",
         scores: [4, 1, 5, 5, 5, 3, 3, 1, 5, 5]
     },
     {
         name: "Kate Austen",
-        photo: "https://geniusvets.s3.amazonaws.com/gv-cat-breeds/Maine-Coon-1.jpg",
+        photo: "https://fastly.4sqi.net/img/general/200x200/391523811_Bet-Oh__l0sWlIevhIxMldtnc7ODGtBG6RhPwxo4WRs.jpg",
         scores: [3, 3, 3, 5, 3, 3, 3, 1, 5, 1]
     },
     {
         name: "Jack Shephard",
-        photo: "https://mainecoons.ca/assets/20180822_182608-670x1000.jpg",
+        photo: "https://irp-cdn.multiscreensite.com/6630b9ff/dms3rep/multi/mobile/black+silver+classic+male+10+months+old.jpg",
         scores: [2, 1, 5, 5, 2, 2, 3, 4, 6, 1]
     },
     {
         name: "John Locke",
-        photo: "https://downeast.com/wp-content/uploads/2013/12/CoonCat_HillarySteinau_Cover-1050x700.jpg",
+        photo: "https://fastly.4sqi.net/img/general/200x200/U12M5ZV4VDBZ3E4W5PMEGZVHT5YAODMVISCPAXSDDYMNUWXN.jpg",
         scores: [4, 5, 5, 4, 3, 3, 2, 4, 5, 2]
     },
     {
         name: "Claire de Ravin",
-        photo: "https://i2.wp.com/metro.co.uk/wp-content/uploads/2018/07/6.png?quality=90&strip=all&zoom=1&resize=540%2C524&ssl=1",
+        photo: "https://breedscat.com/wp-content/uploads/2016/12/maine-coon2-e1483021366694.jpg",
         scores: [4, 1, 5, 5, 1, 3, 3, 3, 4, 1]
     },
     {
         name: "Philip Scott",
-        photo: "http://cfa.org/portals/0/Images/breeds/MC/article1.jpg",
+        photo: "https://fastly.4sqi.net/img/general/200x200/391523811_erJ6QQeKmStMRCsFcEvYNjZ_g7SBMjgFXNULsVQdvO4.jpg",
         scores: [3, 1, 3, 5, 3, 2, 2, 4, 2, 2]
     }
-,{name:"Ada",photo:"",scores:[2,3,3,3,5,5,1,4,4,4]}//insert
+//insert
 ]
 module.exports = friends;
