140f34c8e5d2c345574af5270fc2b0c51976a32b
Vicky Law <vicky.law1217@gmail.com>
2019-09-14 20:58:29 -0400

    modal popups install and display

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,41 +1,39 @@
 let friends = [
     {
         name: "Biily White",
-        photo: "https://www.pets4homes.co.uk/images/articles/4846/large/ten-interesting-facts-about-maine-coon-cats-5b35fc1725375.jpg",
+        photo: "https://static.boredpanda.com/blog/wp-content/uploads/2015/11/maine-coon-cats-19__605.jpg",
         scores: [1, 1, 5, 5, 3, 3, 3, 4, 5, 1]
     },
     {
         name: "Amy Spencer",
-        photo: "",
+        photo: "https://mymodernmet.com/wp/wp-content/uploads/2019/01/maine-coon-cat-photography-26.jpg",
         scores: [4, 1, 5, 5, 5, 3, 3, 1, 5, 5]
     },
     {
         name: "Kate Austen",
-        photo: "",
+        photo: "https://geniusvets.s3.amazonaws.com/gv-cat-breeds/Maine-Coon-1.jpg",
         scores: [3, 3, 3, 5, 3, 3, 3, 1, 5, 1]
     },
     {
         name: "Jack Shephard",
-        photo: "",
+        photo: "https://mainecoons.ca/assets/20180822_182608-670x1000.jpg",
         scores: [2, 1, 5, 5, 2, 2, 3, 4, 6, 1]
     },
     {
         name: "John Locke",
-        photo: "",
+        photo: "https://downeast.com/wp-content/uploads/2013/12/CoonCat_HillarySteinau_Cover-1050x700.jpg",
         scores: [4, 5, 5, 4, 3, 3, 2, 4, 5, 2]
     },
     {
         name: "Claire de Ravin",
-        photo: "",
+        photo: "https://i2.wp.com/metro.co.uk/wp-content/uploads/2018/07/6.png?quality=90&strip=all&zoom=1&resize=540%2C524&ssl=1",
         scores: [4, 1, 5, 5, 1, 3, 3, 3, 4, 1]
     },
     {
         name: "Philip Scott",
-        photo: "",
+        photo: "http://cfa.org/portals/0/Images/breeds/MC/article1.jpg",
         scores: [3, 1, 3, 5, 3, 2, 2, 4, 2, 2]
     }
-
-
-,{name:"Samatha",photo:"gdgdgf",scores:[3,5,4,5,2,1,3,1,1,2]},{name:"Samatha2",photo:"gdgdgf2",scores:[4,5,4,5,2,1,3,1,1,5]}//insert
+,{name:"Ada",photo:"",scores:[2,3,3,3,5,5,1,4,4,4]}//insert
 ]
 module.exports = friends;
--- app/routings/apiRoutes.js
+++ app/routings/apiRoutes.js
@@ -1,14 +1,16 @@
 let friends = require("../data/friends.js");
 let path = require("path");
-var fs = require('fs');
+let fs = require('fs');
+//let popupS = require("popups");
+
 
 // Get data from api friends and show it as json 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
-    
-// api post request
+
+    // api post request
     app.post("/api/friends", function (req, res) {
         let totalDifference = 0;
         let bestMatch = {
@@ -21,8 +23,8 @@ module.exports = function (app) {
         let userName = userData.name;
         let userPhoto = userData.photo;
         let userScores = userData.scores;
-        
-        let x = userScores.map(function (item){
+
+        let x = userScores.map(function (item) {
             return parseInt(item, 10)
         });
 
@@ -32,8 +34,6 @@ module.exports = function (app) {
             scores: x
         };
 
-        
-
         console.log("Name: " + userName);
         console.log("User Score " + userScores);
 
@@ -41,9 +41,9 @@ module.exports = function (app) {
         console.log("Sum of users score " + sum);
         console.log("Best match friend " + bestMatch.friendDifference);
         console.log("-------------------------------------------------")
-        
+
         //Loop all the friends possibility in the database
-        for (var i = 0; i < friends.length; i++){
+        for (var i = 0; i < friends.length; i++) {
             console.log(friends[i].name);
             totalDifference = 0;
             console.log("Total Difference " + totalDifference);
@@ -54,7 +54,7 @@ module.exports = function (app) {
             totalDifference += Math.abs(sum - bfriendScore);
             console.log("--------------------------------------------")
 
-        // Sum of the differences with the best match
+            // Sum of the differences with the best match
             if (totalDifference <= bestMatch.friendDifference) {
                 bestMatch.name = friends[i].name;
                 bestMatch.photo = friends[i].photo;
@@ -63,16 +63,16 @@ module.exports = function (app) {
             console.log(totalDifference + " Total Different");
         }
 
-    //Save the users data to the database, return a json with the best match
+        //Save the users data to the database, return a json with the best match
         console.log(bestMatch);
         friends.push(userData);
         console.log("New User added");
- 
+
 
         var fs = require("fs");
 
 
-        var addData = ",{\rname:\"" + userName + "\",\rphoto:\"" + userPhoto + "\",\rscores:[" + userScores+"]\r}\r"
+        var addData = ",{\rname:\"" + userName + "\",\rphoto:\"" + userPhoto + "\",\rscores:[" + userScores + "]\r}\r"
         var cursor = "//insert";
         addData += cursor;
         fs.readFile("app/data/friends.js", "utf-8", function (err, data) {
@@ -91,16 +91,16 @@ module.exports = function (app) {
             });
         });
 
-
-
-
-        
         console.log(userData);
         res.json(bestMatch);
-        
-    });
 
     
+
+
+
+
+    });
+
 };
 
 
@@ -111,54 +111,3 @@ module.exports = function (app) {
 
 
 
-
-/*const express = require("express");
-
-const path = require("path");
-
-// Sets up the Express App
-// =============================================================
-const app = express();
-
-const PORT = 3001;
-
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("/", function (req, res) {      
-        res.sendFile(path.join(__dirname, "../public/survey.html"))
-});
-let friends = {
-    name: "",
-    photo: "",
-    scores: []
-};
-
-
-// Display a JSON of all possible friends
-/*
-app.get("/add/friends", function (req, res) {
-    return res.json(friends)
-});
-*/
-// New friend created to append
-
-/*app.post("/add/friends", function (req, res) {
-    let newFriends = req.body;
-    //newFriends.routeName = newFriends.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newFriends);
-
-    //friends.push(newFriends);
-
-    res.json(newFriends);
-    //return res.json(newFriends)
-});
-
-
-
-
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
-});*/
